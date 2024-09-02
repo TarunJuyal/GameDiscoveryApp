deef3fd13939cd8d375c87bd8327252c01132e75
Tarun Juyal <tarunjuyalhere@gmail.com>
2024-09-02 13:50:11 +0530

    Hide env variables

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -3,6 +3,6 @@ import axios from "axios";
 export default axios.create({
   baseURL: "https://api.rawg.io/api",
   params: {
-    key: "2c9f06f3206843bebb368bfbf2043f6d",
+    key: import.meta.env.VITE_RAWG_API_KEY,
   },
 });
